0c90e28d88be3a6f6ebec06956bdb4b10d810db5
guhdobraga <guhdobraga@gmail.com>
2023-10-20 12:57:20 -0300

    Commit: Atualização nos arquivos e conexão com db

--- logout.php
+++ logout.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logout</title>
-</head>
-<body>
-    <h1>Logout</h1>
-</body>
-</html>
+<?php 
+session_start();
+session_destroy();
+header('location:login.php');
+exit;
+?>
--- registro.php
+++ registro.php
@@ -1,3 +1,31 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $usuario = $_POST['usuario'];
+    $senha = password_hash($_POST['senha'], PASSWORD_BCRYPT);
+    $email = $_POST['email'];
+
+    try {
+        $pdo = new PDO("mysql:host=localhost;dbname=autenticacao", "root", "");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Erro de conexão com o banco de dados: " . $e->getMessage(
+        ));
+    }
+
+
+    $stmt = $pdo->prepare("INSERT INTO usuarios (email, senha
+    , usuario) VALUES (?, ?, ?)");
+    $stmt->execute([$email, $senha, $usuario]);
+
+
+    $_SESSION['usuario'] = $usuario;
+    header("Location: login.php");
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -6,6 +34,14 @@
     <title>Registro</title>
 </head>
 <body>
-    <h1>Registro</h1>
+    <h2>Registro</h2>
+    <form method="post">
+        <input type="text" name="usuario" placeholder="Nome de Usuário" required><br>
+        <input type="password" name="senha" placeholder="Senha" required><br>
+        <input type="email" name="email" placeholder="Email" required>
+        <br>
+        <input type="submit" value="Cadastrar">
+        
+</form>
 </body>
 </html>
